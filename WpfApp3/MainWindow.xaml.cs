b642a7cd5056363d93ec13b5d50e16e3448cc408
Daniel Dereniowski <leinad885@o2.pl>
2021-03-20 12:49:05 +0100

    "Form, players add, export to file"

--- WpfApp3/MainWindow.xaml.cs
+++ WpfApp3/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace WpfApp3
 {
@@ -21,23 +22,87 @@ namespace WpfApp3
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string file = "repo.txt";
         public MainWindow()
         {
             InitializeComponent();
         }
-        private void add_button_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show($"{name_text_box.Text}");
-        }
-
+        
         private void age_slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
+            var r = age_slider.Value % 10;
+
             age_text_block.Text = age_slider.Value.ToString();
+
+            if (r == 2 || r == 3 || r == 4)
+            {
+                age_text_block.Text += " lata";
+            }
+            else
+            {
+                age_text_block.Text += " lat";
+            }
         }
 
         private void weight_slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-            weight_text_block.Text = weight_slider.Value.ToString();
+            weight_text_block.Text = weight_slider.Value + " kg";
+        }
+
+        private void add_button_Click(object sender, RoutedEventArgs e)
+        {
+            int height;
+
+            if (int.TryParse(height_text_box.Text, out height)) // Zmienna height posiada juz wartość z formularza 'out height'
+            {
+                if (name_text_box.Text != "" & surname_text_box.Text != "")
+                {
+                    Player player = new Player(name_text_box.Text, surname_text_box.Text, (int)age_slider.Value, (int)weight_slider.Value, height, club_combo_box.Text);
+                    bool not_exists = true;
+
+                    foreach (Player i in players_list_box.Items)
+                    {
+                        if (i.Name == player.Name && i.Surname == player.Surname && i.Age == player.Age)
+                        {
+                            not_exists = false;
+                            break;
+                        }
+                    }
+
+                    if (not_exists)
+                    {
+                        players_list_box.Items.Add(player);
+                        clearForm();
+                    }
+                    else MessageBox.Show("Piłkarz znajduję się już na liście");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Wzrost zawiera błędne dane");
+            }
+        }
+
+        public void clearForm()
+        {
+            name_text_box.Text = "";
+            surname_text_box.Text = "";
+            height_text_box.Text = "";
+            age_slider.Value = age_slider.Minimum;
+            weight_slider.Value = weight_slider.Minimum;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            foreach (Player p in players_list_box.Items)
+            {
+                File.AppendAllText(file, p.ToString() + Environment.NewLine);
+            }
         }
     }
 }
--- WpfApp3/Player.cs
+++ WpfApp3/Player.cs
@@ -27,18 +27,33 @@ namespace WpfApp3
         public string Surname { get; set; }
         public int Age { get; set; }
         public int Weight { get; set; }
+        public int Height { get; set; }
+        public string Club { get; set; }
 
-        public Player(string name, string surname, int age, int weight)
+        public Player(string name, string surname, int age, int weight, int height, string club)
         {
             Name = name;
             Surname = surname;
             Age = age;
             Weight = weight;
+            Height = height;
+            Club = club;
         }
 
         public override string ToString()
         {
-            return $"{Name} {Surname} {Age} lat, {Weight} kg";
+            return $"{Name} {Surname} {Age} lat, {Weight} kg, {Height} cm, {Club}";
+        }
+
+        public override bool Equals(object obj)
+        {
+            var p = obj as Player;
+            if (p == null) return false;
+            if (p.Name != this.Name) return false;
+            if (p.Surname != this.Surname) return false;
+            if (p.Age != this.Age) return false;
+            if (p.Club != this.Club) return false;
+            return true;
         }
     }
 }
